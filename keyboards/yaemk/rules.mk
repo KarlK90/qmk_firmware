9ebac88dc229872cd6ca3243bf4475899640df3c
Stefan Kerkmann <karlk90@pm.me>
2021-04-13 23:33:07 +0200

    Fix coil whineing on sh1107 display by lowering precharge periode

--- keyboards/yaemk/rules.mk
+++ keyboards/yaemk/rules.mk
@@ -1,14 +1,13 @@
 # Choose to use RISC-V or ARM MCU
-RISC-V = no
+RISC-V = yes
 
 ifeq ($(strip $(RISC-V)), yes)
-    MCU        =   GD32VF103
-    BOARD      =   SIPEED_LONGAN_NANO
-    LTO_ENABLE =   yes
-    EXTRAFLAGS += -Os
-    EXTRAFLAGS += -fno-tree-loop-distribute-patterns -fno-builtin
+      MCU                =   GD32VF103
+      BOARD              =   SIPEED_LONGAN_NANO
+      LTO_ENABLE         =   no
+      EXTRAFLAGS         += -Os
     # Default clock is 96MHz, 120MHz is out of spec but possible.
-    # OPT_DEFS += -DOVERCLOCK_120MHZ 
+    # OPT_DEFS           += -DOVERCLOCK_120MHZ
 else
     MCU        =   STM32F303
     BOARD      =   QMK_PROTON_C
@@ -30,7 +29,7 @@ DEBOUNCE_TYPE      = eager_pk
 EEPROM_DRIVER      = i2c
 ENCODER_ENABLE     = yes
 EXTRAKEY_ENABLE    = yes
-KEYBOARD_SHARED_EP = no # Enable if you want to use VIA with GD32VF103
+KEYBOARD_SHARED_EP = no
 MOUSEKEY_ENABLE    = yes
 NKRO_ENABLE        = yes
 OLED_DRIVER_ENABLE = yes
@@ -41,16 +40,3 @@ SPLIT_KEYBOARD     = yes
 VIA_ENABLE         = no
 WPM_ENABLE         = yes
 WS2812_DRIVER      = pwm
-
-ifeq ($(strip $(OLED_DRIVER_ENABLE)), yes)
-    ifdef OCEAN_DREAM_ENABLE
-        ifeq ($(strip $(OCEAN_DREAM_ENABLE)), yes)
-            SRC += ocean_dream.c
-            OPT_DEFS += -DOCEAN_DREAM_ENABLE
-        endif
-    endif
-    ifndef OCEAN_DREAM_ENABLE
-        SRC += ocean_dream.c
-        OPT_DEFS += -DOCEAN_DREAM_ENABLE
-    endif
-endif
