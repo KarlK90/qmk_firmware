a5f7cb852f9e0b83fa58177c97edb76e2a2cd5d6
Stefan Kerkmann <karlk90@pm.me>
2021-05-04 10:32:41 +0200

    Update some keys in default and via

--- keyboards/yaemk/keymaps/default/keymap.c
+++ keyboards/yaemk/keymaps/default/keymap.c
@@ -21,8 +21,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_BASE] = LAYOUT( \
      KC_ESC         , KC_1          , KC_2          , KC_3          , KC_4          , KC_5          , LCTL(KC_GRV)  ,                                 KC_ESC        , KC_6          , KC_7          , KC_8          , KC_9          , KC_0          , KC_MINS       ,\
      KC_TAB         , KC_Q          , KC_W          , KC_E          , KC_R          , KC_T          , MO(_RGB)      ,                                 MO(_RGB)      , KC_Y          , KC_U          , KC_I          , KC_O          , KC_P          , KC_LBRC       ,\
-     KC_CAPS        , LGUI_T(KC_A)  , LALT_T(KC_S)  , LCTL_T(KC_D)  , LT(_SYM,KC_F) , KC_G          , MO(_FN)       ,                                 MO(_FN)       , KC_H          , LT(_SYM, KC_J), LCTL_T(KC_K)  , LALT_T(KC_L)  ,LGUI_T(KC_SCLN), KC_QUOT       ,\
-     KC_NUBS        , KC_Z          , KC_X          , KC_C          , KC_V          , KC_B          , KC_BSPC       , KC_BSPC       , KC_ENT        , KC_ENT        , KC_N          , KC_M          , KC_COMM       , KC_DOT        , KC_SLSH       , KC_NUBS       ,\
+     MO(_SYM)       , LGUI_T(KC_A)  , LALT_T(KC_S)  , LCTL_T(KC_D)  , LT(_SYM,KC_F) , KC_G          , MO(_FN)       ,                                 MO(_FN)       , KC_H          , LT(_SYM, KC_J), LCTL_T(KC_K)  , LALT_T(KC_L)  ,LGUI_T(KC_SCLN), KC_QUOT       ,\
+     MO(_NAV)       , KC_Z          , KC_X          , KC_C          , KC_V          , KC_B          , KC_BSPC       , MO(_NAV)      , MO(_NAV)      , KC_ENT        , KC_N          , KC_M          , KC_COMM       , KC_DOT        , KC_SLSH       , MO(_NAV)      ,\
                                                       TG(_GAME)     , KC_ESC        , LSFT_T(KC_SPC), KC_BSPC       , MO(_NAV)      , MO(_NAV)      , KC_ENT        , RSFT_T(KC_SPC), KC_RALT       , TG(_GAME)\
   ),
    [_SYM] = LAYOUT( \
