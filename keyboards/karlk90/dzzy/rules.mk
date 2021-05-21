8e7316835a72827268a93727baeb9c0450cd10bc
Stefan Kerkmann <karlk90@pm.me>
2021-05-21 23:56:38 +0200

    Add dzzy keyboard

---
+++ keyboards/karlk90/dzzy/rules.mk
@@ -0,0 +1,38 @@
+# Choose to use RISC-V or ARM MCU
+RISC-V = yes
+
+ifeq ($(strip $(RISC-V)), yes)
+    MCU                =   GD32VF103
+    BOARD              =   SIPEED_LONGAN_NANO
+    LTO_ENABLE         =   yes
+    EXTRAFLAGS         += -O3  -gdwarf-4 -Wextra
+    # Default clock is 96MHz, 120MHz is out of spec but possible.
+    OPT_DEFS           += -DOVERCLOCK_120MHZ
+else
+    MCU        =   STM32F303
+    BOARD      =   GENERIC_STM32_F303XC
+    LTO_ENABLE =   yes
+    EXTRAFLAGS += -O3
+endif
+
+# DZZY source files
+SRC     += matrix.c
+LAYOUTS =  dzzy_5x6
+
+# Debugging
+EXTRAFLAGS     += -g
+ALLOW_WARNINGS =   yes
+
+# QMK features 
+CONSOLE_ENABLE                     = no
+CUSTOM_MATRIX                      = lite
+DEBOUNCE_TYPE                      = asym_eager_defer_pk
+EEPROM_DRIVER                      = transient
+ENCODER_ENABLE                     = no
+EXTRAKEY_ENABLE                    = no
+KEYBOARD_SHARED_EP                 = yes
+MOUSEKEY_ENABLE                    = no
+NKRO_ENABLE                        = yes
+REGISTER_MULTIPLE_KEYEVENTS_ENABLE = yes
+VIA_ENABLE                         = no
+WPM_ENABLE                         = no
