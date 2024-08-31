3d52220984ad99565dd2a822f0410d751a40c368
Stefan Kerkmann <karlk90@pm.me>
2024-08-31 14:55:24 +0200

    boards: rp2350: add rp2350 support

---
+++ platforms/chibios/boards/GENERIC_PROMICRO_RP2350/configs/board.h
@@ -0,0 +1,11 @@
+// Copyright 2022 Stefan Kerkmann
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include_next <board.h>
+
+#define BOARD_GENERIC_PROMICRO_RP2350
+
+#undef BOARD_NAME
+#define BOARD_NAME "Pro Micro RP2350"
